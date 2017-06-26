5acb256783bb898fd9738c8b08be771202d43c9c
Yasmine Abulhab <yabulhab@gmail.com>
2017-06-26 11:30:26 -0700

    added the repo name to the hostname in config

--- config.js
+++ config.js
@@ -4,13 +4,11 @@ console.log(x)
 let hostname = window.location.hostname;
 console.log("hostname is " + hostname);
 
-if (hostname === "yabulhab.github.io") {
+if (hostname === "yabulhab.github.io/eye2earmusic") {
 	window._ROOT_FILE_PATH_ = "/eye2earmusic";
-	console.log("yes, the hostname is" + hostname)
 } else if (hostname === "localhost") {
 	window._ROOT_FILE_PATH_ = ".";
-
 } else {
-	window._ROOT_FILE_PATH_ = "./eye2earmusic";
+	window._ROOT_FILE_PATH_ = "/eye2earmusic";
 	console.log("I'm confused. Where are we?");
 }
