9929754227db04c3b2cc9f848c0d83cb30d9ef5f
Yasmine Abulhab <yabulhab@gmail.com>
2017-06-26 11:31:15 -0700

    nope took it out

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ console.log(x)
 let hostname = window.location.hostname;
 console.log("hostname is " + hostname);
 
-if (hostname === "yabulhab.github.io/eye2earmusic") {
+if (hostname === "yabulhab.github.io") {
 	window._ROOT_FILE_PATH_ = "/eye2earmusic";
 } else if (hostname === "localhost") {
 	window._ROOT_FILE_PATH_ = ".";
