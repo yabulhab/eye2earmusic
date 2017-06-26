c8578ca1fafc3da8fae497aee45af05b9d6accdb
Yasmine Abulhab <yabulhab@gmail.com>
2017-06-25 20:01:15 -0400

    fixed url for localhost on config.sj

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ console.log("hostname is " + hostname);
 
 if (hostname === "https://yabulhab.github.io/eye2earmusic/") {
 	window._ROOT_FILE_PATH_ = "/eye2earmusic";
-} else if (hostname === "client?https://127.0.0.1:8080/") {
+} else if (hostname === "localhost") {
 	window._ROOT_FILE_PATH_ = ".";
 } else {
 	console.log("I'm confused. Where are we?");
