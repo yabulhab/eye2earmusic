a6aa033966b53030a0db2fb24936fb47d0b5a71a
Yasmine Abulhab <yabulhab@gmail.com>
2017-06-26 11:18:00 -0700

    added console.log error

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ console.log("hostname is " + hostname);
 
 if (hostname === "yabulhab.github.io") {
 	window._ROOT_FILE_PATH_ = "/eye2earmusic";
+	console.log("yes, the hostname is" + hostname)
 } else if (hostname === "localhost") {
 	window._ROOT_FILE_PATH_ = ".";
 
